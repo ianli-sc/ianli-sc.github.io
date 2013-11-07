dab65231839bd40a7dbde46f6b04e98d377c5082
承风 <libiao.lb@taobao.com>
2013-11-07 12:07:26 +0800

    create basic gallery

--- javascripts/applet/gallery/index.js
+++ javascripts/applet/gallery/index.js
@@ -19,10 +19,42 @@ define(['jquery'], function() {
 
     gallery.prototype = {
         _init : function() {
-            
+            var cssloaded = false;
+            $('head link').each(function(index, item){
+                if(item.href.match(/javascripts\/applet\/gallery\/base.css/)){
+                    cssloaded = true;
+                    return false;
+                }
+            });
+            if(!cssloaded) {
+                var link = document.createElement("link");
+                link.type = 'text/css';
+                link.rel = 'stylesheet';
+                link.href = '../javascripts/applet/gallery/base.css';
+                $("head").prepend(link);
+            }
+            var data = this.data;
+            var container = '<article class="gallery-container">';
+                /*<ul class="gallery-ctr">
+                    <li class="ctr-matrix"></li>
+                    <li class="ctr-expand"></li>
+                </ul>';*/
+            $.each(data, function(key, value) {
+                var html = '<section class="gallery-item">\
+                    <div class="item-content">\
+                        <img class="item-data" src="' + value.url + '">\
+                        <cite class="item-title">' + value.title + '</cite>\
+                    </div>\
+                    <div class="item-describe">\
+                        <h4>' + value.describe + '</h4>\
+                    </div>\
+                </section>';
+                container += html;
+            });
+            container += '</article>';
+            $(container).appendTo($(this.root));
         },
         show : function() {
-            alert(123);
         }
     };
 
