89c651707262c9b09c1399e6fbab878e84b8482e
ianli <ian.primary.li@gmail.com>
2022-12-25 06:35:39 +0800

    feat: 简单的移动端适配

--- .umirc.ts
+++ .umirc.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "umi";
+import px2rem from "postcss-plugin-px2rem";
 
 export default defineConfig({
   npmClient: 'pnpm',
+  extraPostCSSPlugins: [
+    px2rem({
+      rootValue: 18.75,
+    })
+  ]
 });
