cf12288384a7be616c6a1980aefe86aa2056069c
ianli <ian.primary.li@gmail.com>
2022-12-25 07:37:17 +0800

    feat: 广告区域完成

--- src/components/ads/index.tsx
+++ src/components/ads/index.tsx
@@ -2,13 +2,13 @@ import styles from './index.less';
 import { useState } from 'react';
 
 export default function ADS() {
-    const [btnText, setBtnText] = useState('more');
+    const [btnText, setBtnText] = useState('More');
     const changeBtnText = () => {
-        setBtnText(btnText === 'more' ? 'less' : 'more');
+        setBtnText(btnText === 'More' ? 'Less' : 'More');
     }
     return <div className={styles.ads}>
-        <div className={styles.text}>
-        BLACK FRIDAY SALE | Up to 50% off FW22 with an extra 10% off using code ‘BLACK10’ at checkout. T&Cs apply
+        <div className={btnText === 'Less' ? styles.text : styles['text-inline']}>
+        BLACK FRIDAY SALE | Up to 50% off FW22 with an extra 10% off using code ‘BLACK10’ at checkout. <span className={styles.link}>T&Cs apply</span>
         </div>
         <div className={styles.btn} onClick={changeBtnText}>{btnText}</div>
     </div>
